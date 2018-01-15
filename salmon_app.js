80ea47949bdd969a8e119e0babf255d01f7da0c0
zachary schumpert <buphnezz@gmail.com>
2018-01-15 14:25:14 -0800

    created all shops in html, created hoursOfOperation global variable

---
+++ salmon_app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// create a shop object
+// stores min, max and average cookies per customer in the object properties
+// use a method of the shop object to genereate a random number of cookies per hour
+//Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
+//Store the results for each location in a separate array...perhaps as a property of the object representing that location
+//Display the values of each array as unordered lists in the browser
+
+// all shops are open from 6am to 8pm so I made it a global variable
+var hoursOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm']; 
+
+// 1.  1st and Pike shop object literal
+var firstAndPike = {
+  shopInfo: ['minCustomers', 'maxCustomers', ]
+  
+  minCustomers: ,
+  maxCustomers: ,
+  avgCookiesPerCustomer: 
+}
+
+
+// 2.  SeaTac Airport   shop object literal
+// 3. Seattle Center shop object literal
+// 4. Capital Hill shop object literal
+// 5. Ali  shop object literal
+
+
