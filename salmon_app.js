a856f5ecafade04267a976ce710311b1645da077
zachary schumpert <buphnezz@gmail.com>
2018-01-15 15:41:00 -0800

    using the .textContent method, I now have each hour listed and the ranom num of customers displayed next to it

--- salmon_app.js
+++ salmon_app.js
@@ -12,10 +12,24 @@ var hoursOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm
 
 // need the number of customers each hour, so let's make a random number generator
 var randomNumOfCustomers = Math.floor(Math.random() * 150);
+var randomNumOfCustomers2 = Math.floor(Math.random() * 150);
+var randomNumOfCustomers3 = Math.floor(Math.random() * 150);
+var randomNumOfCustomers4 = Math.floor(Math.random() * 150);
+var randomNumOfCustomers5 = Math.floor(Math.random() * 150);
+var randomNumOfCustomers6 = Math.floor(Math.random() * 150);
+var randomNumOfCustomers7 = Math.floor(Math.random() * 150);
+var randomNumOfCustomers8 = Math.floor(Math.random() * 150);
+var randomNumOfCustomers9 = Math.floor(Math.random() * 150);
+var randomNumOfCustomers10 = Math.floor(Math.random() * 150);
+var randomNumOfCustomers11 = Math.floor(Math.random() * 150);
+var randomNumOfCustomers12 = Math.floor(Math.random() * 150);
+var randomNumOfCustomers13 = Math.floor(Math.random() * 150);
+var randomNumOfCustomers14 = Math.floor(Math.random() * 150);
+var randomNumOfCustomers15 = Math.floor(Math.random() * 150);
 
 // 1.  1st and Pike shop object literal
 var firstAndPike = {
-  customers: [randomNumOfCustomers, randomNumOfCustomers,],
+  customers: [randomNumOfCustomers, randomNumOfCustomers2, randomNumOfCustomers3, randomNumOfCustomers4, randomNumOfCustomers5, randomNumOfCustomers6, randomNumOfCustomers7, randomNumOfCustomers8, randomNumOfCustomers9, randomNumOfCustomers10, randomNumOfCustomers11, randomNumOfCustomers12, randomNumOfCustomers13, randomNumOfCustomers14, randomNumOfCustomers15],
 //   shopInfo: ['minCustomers', 'maxCustomers', 'avgNumOfCookiesPurchased']
 render: function () {
   var ulEl = document.getElementById('firstAndPikeShop');
@@ -24,7 +38,7 @@ render: function () {
     // 1. create a list of items (create the element)
     var liEl = document.createElement('li');
     // 2. give the li element content
-    liEl.textContent = this.customers[i];
+    liEl.textContent = hoursOfOperation[i] + ': ' + this.customers[i];
     // 3. append the li to the ul (append the element to the DOM)
     ulEl.appendChild(liEl);
   }
