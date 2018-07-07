d35224e8baf590fc6932d15239f59fa14bb21b81
zachary schumpert <buphnezz@gmail.com>
2018-07-06 23:17:32 -0700

    refactoring-to-es6

--- salmon-constructors.app.js
+++ salmon-constructors.app.js
@@ -1,30 +1,30 @@
-var hoursOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+let hoursOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 
 // We need an array to hold our dogs
-var allStores = [];
+let allStores = [];
 
 // We need to access the table that is in the DOM
-var storeTable = document.getElementById('stores');
+let storeTable = document.getElementById('stores');
 
 // We need to access the form from our index.html
-var storeForm = document.getElementById('store-form');
+let storeForm = document.getElementById('store-form');
 console.log('Storeform test', storeForm);
 
-function totalCookieFooter() {
-  var tfootEl = document.createElement('tfoot'); // created a tfoot in my html doc
+const totalCookieFooter = () => {
+  let tfootEl = document.createElement('tfoot'); // created a tfoot in my html doc
   console.log(tfootEl);
-  var trEl = document.createElement('tr'); // created a <tr> in my html doc
-  var tdEl = document.createElement('td');  // created a <td> in my html doc
+  let trEl = document.createElement('tr'); // created a <tr> in my html doc
+  let tdEl = document.createElement('td');  // created a <td> in my html doc
   console.log(tdEl);
   tdEl.textContent = 'Total Cookies Sold Per Hour';  // inputting the data 'Total Cookies' into the <td> we just created in the HTML doc.
   trEl.appendChild(tdEl); //  append that Total Cookies data to the <tr> above it.
 
 
-  for (var hour = 0; hour < hoursOfOperation.length; hour++) {
+  for (let hour = 0; hour < hoursOfOperation.length; hour++) {
     console.log(hoursOfOperation[hour]);
-    var runningTotal = 0;
-    for (var store = 0; store < allStores.length; store++) {
+    let runningTotal = 0;
+    for (let store = 0; store < allStores.length; store++) {
       console.log(allStores[store]);
       allStores[store].salesPerEachHour[hour];
       console.log(allStores[store].salesPerEachHour[hour]);
@@ -32,20 +32,20 @@ function totalCookieFooter() {
       console.log(runningTotal);
     }
 
-    var tdEl = document.createElement('td');  // created a <td> in my html doc
+    let tdEl = document.createElement('td');  // created a <td> in my html doc
     console.log(tdEl);
     tdEl.textContent = runningTotal;  // inputting the data 'Total Cookies' into the <td> we just created in the HTML doc.
     trEl.appendChild(tdEl); //  append that Total Cookies data to the <tr> above it.
   }
 
-  var grandTotal = 0;
-  for (var i = 0; i < allStores.length; i++) {
+  let grandTotal = 0;
+  for (let i = 0; i < allStores.length; i++) {
     console.log('this shows all stores', allStores[i]);
     console.log(allStores[i].totalCookies);
     grandTotal += allStores[i].totalCookies;
     console.log(grandTotal);
   }
-  var tdEl = document.createElement('td');  // created a <td> in my html doc
+  let tdEl = document.createElement('td');  // created a <td> in my html doc
   console.log(tdEl);
   tdEl.textContent = grandTotal;  // inputting the data 'Total Cookies' into the <td> we just created in the HTML doc.
   trEl.appendChild(tdEl); //  append that Total Cookies data to the <tr> above it.
@@ -55,7 +55,7 @@ function totalCookieFooter() {
 }
 
 // We need a constructor to make our dog objects
-function Store(nameOfStore, minCustomers, maxCustomers, avgCookiesSold) {
+const Store = (nameOfStore, minCustomers, maxCustomers, avgCookiesSold) => {
   this.nameOfStore = nameOfStore;
   this.minCustomers = minCustomers;
   this.avgCookiesSold = avgCookiesSold;
@@ -63,15 +63,15 @@ function Store(nameOfStore, minCustomers, maxCustomers, avgCookiesSold) {
   this.salesPerEachHour = [];
   this.totalCookies = 0;
 
-  this.numOfCustomers = function () {
-    var avgFirstandPikeCustomers = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1) + this.minCustomers)
+  this.numOfCustomers = () => {
+    let avgFirstandPikeCustomers = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1) + this.minCustomers)
     return avgFirstandPikeCustomers;
   };
 
-  this.cookiesSoldPerEachHour = function () {
-    for (var cookieCounter = 0; cookieCounter < hoursOfOperation.length; cookieCounter++) {
+  this.cookiesSoldPerEachHour = () => {
+    for (let cookieCounter = 0; cookieCounter < hoursOfOperation.length; cookieCounter++) {
 
-      var avgCookiesPurchased = Math.floor(this.numOfCustomers() * this.avgCookiesSold);
+      let avgCookiesPurchased = Math.floor(this.numOfCustomers() * this.avgCookiesSold);
       this.salesPerEachHour.push(avgCookiesPurchased)
 
       this.totalCookies = this.totalCookies + avgCookiesPurchased;
@@ -88,18 +88,18 @@ function Store(nameOfStore, minCustomers, maxCustomers, avgCookiesSold) {
 }
 
 // Let's refactor so that render() method is on the constructor's prototype; this will tidy up the way things look on the screen
-Store.prototype.render = function () {
+Store.prototype.render = () => {
   // create tr
-  var trEl = document.createElement('tr');
+  let trEl = document.createElement('tr');
   // create td
-  var tdEl = document.createElement('td');
+  let tdEl = document.createElement('td');
   // give td content (name, then color, then breed, the nickname) <- eventually do it in a for loop
   tdEl.textContent = this.nameOfStore;
   // append td to tr
   trEl.appendChild(tdEl);
   // append tr to table
 
-  for (var i = 0; i < this.salesPerEachHour.length; i++) {
+  for (let i = 0; i < this.salesPerEachHour.length; i++) {
     tdEl = document.createElement('td');
     trEl.appendChild(tdEl);
     tdEl.textContent = this.salesPerEachHour[i];
@@ -114,37 +114,37 @@ Store.prototype.render = function () {
 };
 
 // We need a separate function to make the table header
-function makeHeaderRow() {
-  var trEl = document.createElement('tr');
-  var thEl = document.createElement('th');
+const makeHeaderRow = () => {
+  let trEl = document.createElement('tr');
+  let thEl = document.createElement('th');
   thEl.textContent = '';
   trEl.appendChild(thEl);
 
-  for (var i = 0; i < hoursOfOperation.length; i++) {
-    var thEl = document.createElement('th');
+  for (let i = 0; i < hoursOfOperation.length; i++) {
+    let thEl = document.createElement('th');
     trEl.appendChild(thEl);
     thEl.textContent = hoursOfOperation[i];
-  }
-  var thEl = document.createElement('th');
+  };
+  let thEl = document.createElement('th');
   thEl.textContent = 'Total:';
   trEl.appendChild(thEl);
   storeTable.appendChild(trEl);
-}
+};
 
-function renderAllStores() {
-  for (var i in allStores) {
+const renderAllStores = () => {
+  for (let i in allStores) {
     allStores[i].render();
   }
 }
 
-function addNewStore(event) {
+const addNewStore = (event) => {
   console.log('inside of event');
   event.preventDefault();
   console.log(event.target.storeName.value);
-  var newStoreName = event.target.storeName.value;
-  var newMinCustomers = event.target.minCustomers.value;
-  var newMaxCustomers = event.target.maxCustomers.value;
-  var newAvgCookiesSold = event.target.avgCookiesSold.value;
+  let newStoreName = event.target.storeName.value;
+  let newMinCustomers = event.target.minCustomers.value;
+  let newMaxCustomers = event.target.maxCustomers.value;
+  let newAvgCookiesSold = event.target.avgCookiesSold.value;
 
   new Store(newStoreName, newMinCustomers, newMaxCustomers, newAvgCookiesSold);
 
@@ -159,27 +159,27 @@ alert('The button press worked');
 console.log(addNewStore);
 
 // We need to create our Dog instances
-var firstAndPike = new Store('First and Pike', 23, 65, 6.3);
-var seaTacAirport = new Store('SeaTac Airport', 3, 24, 1.2);
-var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
-var capitolHill = new Store('Capitol Hill', 20, 38, 2.3);
-var alki = new Store('Alki', 2, 16, 4.6);
+let firstAndPike = new Store('First and Pike', 23, 65, 6.3);
+let seaTacAirport = new Store('SeaTac Airport', 3, 24, 1.2);
+let seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
+let capitolHill = new Store('Capitol Hill', 20, 38, 2.3);
+let alki = new Store('Alki', 2, 16, 4.6);
 
-// var table = document.getElementById("stores");
-// var footer = table.createTFoot();
-// var row = footer.insertRow(0);
-// var cell = row.insertCell(0);
+// let table = document.getElementById("stores");
+// let footer = table.createTFoot();
+// let row = footer.insertRow(0);
+// let cell = row.insertCell(0);
 // cell.innerHTML = "<b>Total Cookies Sold</b>";
 
 
 makeHeaderRow();
 renderAllStores();
 totalCookieFooter();
-function myMap() {
-  var mapOptions = {
+const myMap = () => {
+  let mapOptions = {
     center: new google.maps.LatLng(47.608013, -122.335167),
     zoom: 10,
     mapTypeId: google.maps.MapTypeId.HYBRID
   }
-  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-} 
+  let map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}; 
--- salmon_app.js
+++ salmon_app.js
@@ -7,100 +7,100 @@
 //Store the results for each location in a separate array...perhaps as a property of the object representing that location
 //Display the values of each array as unordered lists in the browser
 
-// all shops are open from 6am to 8pm so I made it a global variable
-var hoursOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm']; 
+// all shops are open from 6am to 8pm so I made it a global letiable
+let hoursOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 // I'm currently not using minNumberCookies or maxNumberCookies... I used that equation in min/max number of customers instead.  Go ahead and delete both for now.
-// var minNumberCookies = Math.floor(Math.random() * (50 - 0));
+// let minNumberCookies = Math.floor(Math.random() * (50 - 0));
 
-// var maxNumberCookies = Math.floor(Math.random() * (150 - 51));
+// let maxNumberCookies = Math.floor(Math.random() * (150 - 51));
 
-var avgNumCookiesPurchased = Math.ceil(Math.random()) * (3 - 1);
+let avgNumCookiesPurchased = Math.ceil(Math.random()) * (3 - 1);
 
 // need the number of customers each hour, so let's make a random number generator
 // need avg number of customers per hour * avg number of cookie purchased
-var minNumOfCustomersSixAm = Math.floor(Math.random() * (50 - 0));
-var maxNumOfCustomersSixAm = Math.floor(Math.random() * (150 - 51) + 50);
-var minNumOfCustomersSevenAm = Math.floor(Math.random() * (50 - 0));
-var maxNumOfCustomersSevenAm = Math.floor(Math.random() * (150 - 51) + 50);
-var minNumOfCustomersEightAm = Math.floor(Math.random() * (50 - 0));
-var maxNumOfCustomersEightAm = Math.floor(Math.random() * (150 - 51) + 50);
-var minNumOfCustomersNineAm = Math.floor(Math.random() * (50 - 0));
-var maxNumOfCustomersNineAm = Math.floor(Math.random() * (150 - 51) + 50);
-var minNumOfCustomersTenAm = Math.floor(Math.random() * (50 - 0));
-var maxNumOfCustomersTenAm = Math.floor(Math.random() * (150 - 51) + 50);
-var minNumOfCustomersElevenAm = Math.floor(Math.random() * (50 - 0));
-var maxNumOfCustomersElevenAm = Math.floor(Math.random() * (150 - 51) + 50);
-var minNumOfCustomersTwelvePm = Math.floor(Math.random() * (50 - 0));
-var maxNumOfCustomersTwelvePm = Math.floor(Math.random() * (150 - 51) + 50);
-var minNumOfCustomersOnePm = Math.floor(Math.random() * (50 - 0));
-var maxNumOfCustomersOnePm = Math.floor(Math.random() * (150 - 51) + 50);
-var minNumOfCustomersTwoPm = Math.floor(Math.random() * (50 - 0));
-var maxNumOfCustomersTwoPm = Math.floor(Math.random() * (150 - 51) + 50);
-var minNumOfCustomersThreePm = Math.floor(Math.random() * (50 - 0));
-var maxNumOfCustomersThreePm = Math.floor(Math.random() * (150 - 51) + 50);
-var minNumOfCustomersFourPm = Math.floor(Math.random() * (50 - 0));
-var maxNumOfCustomersFourPm = Math.floor(Math.random() * (150 - 51) + 50);
-var minNumOfCustomersFivePm = Math.floor(Math.random() * (50 - 0));
-var maxNumOfCustomersFivePm = Math.floor(Math.random() * (150 - 51) + 50);
-var minNumOfCustomersSixPm = Math.floor(Math.random() * (50 - 0));
-var maxNumOfCustomersSixPm = Math.floor(Math.random() * (150 - 51) + 50);
-var minNumOfCustomersSevenPm = Math.floor(Math.random() * (50 - 0));
-var maxNumOfCustomersSevenPm = Math.floor(Math.random() * (150 - 51) + 50);
-var minNumOfCustomersEightPm = Math.floor(Math.random() * (50 - 0));
-var maxNumOfCustomersEightPm = Math.floor(Math.random() * (150 - 51) + 50);
+let minNumOfCustomersSixAm = Math.floor(Math.random() * (50 - 0));
+let maxNumOfCustomersSixAm = Math.floor(Math.random() * (150 - 51) + 50);
+let minNumOfCustomersSevenAm = Math.floor(Math.random() * (50 - 0));
+let maxNumOfCustomersSevenAm = Math.floor(Math.random() * (150 - 51) + 50);
+let minNumOfCustomersEightAm = Math.floor(Math.random() * (50 - 0));
+let maxNumOfCustomersEightAm = Math.floor(Math.random() * (150 - 51) + 50);
+let minNumOfCustomersNineAm = Math.floor(Math.random() * (50 - 0));
+let maxNumOfCustomersNineAm = Math.floor(Math.random() * (150 - 51) + 50);
+let minNumOfCustomersTenAm = Math.floor(Math.random() * (50 - 0));
+let maxNumOfCustomersTenAm = Math.floor(Math.random() * (150 - 51) + 50);
+let minNumOfCustomersElevenAm = Math.floor(Math.random() * (50 - 0));
+let maxNumOfCustomersElevenAm = Math.floor(Math.random() * (150 - 51) + 50);
+let minNumOfCustomersTwelvePm = Math.floor(Math.random() * (50 - 0));
+let maxNumOfCustomersTwelvePm = Math.floor(Math.random() * (150 - 51) + 50);
+let minNumOfCustomersOnePm = Math.floor(Math.random() * (50 - 0));
+let maxNumOfCustomersOnePm = Math.floor(Math.random() * (150 - 51) + 50);
+let minNumOfCustomersTwoPm = Math.floor(Math.random() * (50 - 0));
+let maxNumOfCustomersTwoPm = Math.floor(Math.random() * (150 - 51) + 50);
+let minNumOfCustomersThreePm = Math.floor(Math.random() * (50 - 0));
+let maxNumOfCustomersThreePm = Math.floor(Math.random() * (150 - 51) + 50);
+let minNumOfCustomersFourPm = Math.floor(Math.random() * (50 - 0));
+let maxNumOfCustomersFourPm = Math.floor(Math.random() * (150 - 51) + 50);
+let minNumOfCustomersFivePm = Math.floor(Math.random() * (50 - 0));
+let maxNumOfCustomersFivePm = Math.floor(Math.random() * (150 - 51) + 50);
+let minNumOfCustomersSixPm = Math.floor(Math.random() * (50 - 0));
+let maxNumOfCustomersSixPm = Math.floor(Math.random() * (150 - 51) + 50);
+let minNumOfCustomersSevenPm = Math.floor(Math.random() * (50 - 0));
+let maxNumOfCustomersSevenPm = Math.floor(Math.random() * (150 - 51) + 50);
+let minNumOfCustomersEightPm = Math.floor(Math.random() * (50 - 0));
+let maxNumOfCustomersEightPm = Math.floor(Math.random() * (150 - 51) + 50);
 
 
 // 1.  1st and Pike shop object literal
-var firstAndPike = {
+let firstAndPike = {
   // customers: [(maxNumOfCustomersSixAm - minNumOfCustomersSixAm) * avgNumCookiesPurchased, (maxNumOfCustomersSevenAm - minNumOfCustomersSevenAm) * avgNumCookiesPurchased, (maxNumOfCustomersEightAm - minNumOfCustomersEightAm) * avgNumCookiesPurchased, (maxNumOfCustomersNineAm - minNumOfCustomersNineAm) * avgNumCookiesPurchased, (maxNumOfCustomersTenAm - minNumOfCustomersTenAm) * avgNumCookiesPurchased, (maxNumOfCustomersElevenAm - minNumOfCustomersElevenAm) * avgNumCookiesPurchased, (maxNumOfCustomersTwelvePm - minNumOfCustomersTwelvePm) * avgNumCookiesPurchased, (maxNumOfCustomersOnePm - minNumOfCustomersOnePm) * avgNumCookiesPurchased, (maxNumOfCustomersTwoPm - minNumOfCustomersTwoPm) * avgNumCookiesPurchased, (maxNumOfCustomersThreePm - minNumOfCustomersThreePm) * avgNumCookiesPurchased, (maxNumOfCustomersFourPm - minNumOfCustomersFourPm) * avgNumCookiesPurchased, (maxNumOfCustomersFivePm - minNumOfCustomersFivePm) * avgNumCookiesPurchased, (maxNumOfCustomersSixPm - minNumOfCustomersSixPm) * avgNumCookiesPurchased, (maxNumOfCustomersSevenPm - minNumOfCustomersSevenPm) * avgNumCookiesPurchased, (maxNumOfCustomersEightPm - minNumOfCustomersEightPm) * avgNumCookiesPurchased],
-  
+
   // randomNumOfCustomers2, randomNumOfCustomers3, randomNumOfCustomers4, randomNumOfCustomers5, randomNumOfCustomers6, randomNumOfCustomers7, randomNumOfCustomers8, randomNumOfCustomers9, randomNumOfCustomers10, randomNumOfCustomers11, randomNumOfCustomers12, randomNumOfCustomers13, randomNumOfCustomers14, randomNumOfCustomers15],
 //   shopInfo: ['minCustomers', 'maxCustomers', 'avgNumOfCookiesPurchased']
 
 
-salesPerEachHour: [],
-minCustomers: 23,
-maxCustomers: 65,
-avgCookiesSold: 6.3,
+  salesPerEachHour: [],
+  minCustomers: 23,
+  maxCustomers: 65,
+  avgCookiesSold: 6.3,
 
-numOfCustomers: function () {
-  var avgFirstandPikeCustomers = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers) + this.minCustomers)
-  return avgFirstandPikeCustomers;
-},
+  numOfCustomers: function () {
+    let avgFirstandPikeCustomers = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers) + this.minCustomers);
+    return avgFirstandPikeCustomers;
+  },
 
-cookiesSoldPerEachHour: function () {
-  var avgCookiesPurchased = this.numOfCustomers() * this.avgCookiesSold;
-  this.salesPerEachHour.push(avgCookiesPurchased);
-},
+  cookiesSoldPerEachHour: function () {
+    let avgCookiesPurchased = this.numOfCustomers() * this.avgCookiesSold;
+    this.salesPerEachHour.push(avgCookiesPurchased);
+  },
 
-render: function () {
-  var ulEl = document.getElementById('firstAndPikeShop');
+  render: function () {
+    let ulEl = document.getElementById('firstAndPikeShop');
 
-  for (var i = 0; i < hoursOfOperation.length; i++) {
-    
-    this.cookiesSoldPerEachHour();
-    // 1. create a list of items (create the element)
-    var liEl = document.createElement('li');
-    // 2. give the li element content
-    liEl.textContent = hoursOfOperation[i] + ': ' + this.salesPerEachHour[i] + ' cookies';
-    // 3. append the li to the ul (append the element to the DOM)
-    ulEl.appendChild(liEl);
+    for (let i = 0; i < hoursOfOperation.length; i++) {
+
+      this.cookiesSoldPerEachHour();
+      // 1. create a list of items (create the element)
+      let liEl = document.createElement('li');
+      // 2. give the li element content
+      liEl.textContent = hoursOfOperation[i] + ': ' + this.salesPerEachHour[i] + ' cookies';
+      // 3. append the li to the ul (append the element to the DOM)
+      ulEl.appendChild(liEl);
+    }
+
   }
-
-}  
 //   minCustomers: ,
 //   maxCustomers: ,
-//   avgCookiesPerCustomer: 
-}
+//   avgCookiesPerCustomer:
+};
 
 
 // 2.  SeaTac Airport   shop object literal
-var seaTacAirport = {
+let seaTacAirport = {
   minCustomers: 3,
   maxCustomers: 24,
   avgCookiesSold: 1.2,
-}
+};
 // 3. Seattle Center shop object literal
 // 4. Capital Hill shop object literal
 // 5. Ali  shop object literal
